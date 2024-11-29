0270a0c275706b4f372c002ec1314ee3953c9809
chintan <chintan.tops@gmail.com>
2024-11-29 15:32:48 +0530

    all

---
+++ Basic/demo.py
@@ -0,0 +1,28 @@
+
+
+# a = 10 //int
+# b = 10.20 //float
+# c = "Harsh" // str
+# d = 2 + 4j // complex
+# e = [10,20,30,40,50,50] // list
+# f = (1,2,3,4,5,6,7,7) //tuple
+# g = {1,2,3,5,6,7,7} //set
+# h = dict({102:"Python",103:"Java"}) //dict
+
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+
+_a  = 10
+print(_a)
---
+++ Basic/test.py
@@ -0,0 +1,32 @@
+
+# print("Hello \" python",end="")
+# print('Hello " python',end="")
+# print("Hello\b ' python")
+# print("""Hello
+#         Python
+#         Hello 
+#         Java
+# """)
+
+# print(10,20,30,4,50,60,sep="-")
+
+
+# a =int(input("enter number 1 : "))
+# b = int (input("enter number 2 : "))
+# print(a+b)
+
+# print(r"Hello \n python")
+
+# name = "Rashi dave".split('a')
+# print(name)
+
+# fname,lname = input("enter data : ").split()
+
+# print(fname)
+# print(lname)
+
+fname="Harsh"
+lname = "Chauhan"
+
+# print("my name is {1} and surname is {0}".format(fname,lname))
+print(f"my name is {fname} and my surname is {lname}")
