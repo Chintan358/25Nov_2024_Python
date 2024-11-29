b087fdaac8e09cd7cb788b58cb5e754370b8c85a
chintan <chintan.tops@gmail.com>
2024-11-29 16:06:25 +0530

    operators

---
+++ Basic/oprators.py
@@ -0,0 +1,65 @@
+
+# arihmatic
+
+# a = 10
+# b = 20
+
+# print(a+b)
+# print("tops"+"tech")
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(5**3)
+# print(a//b)
+
+# assignment op
+
+# a = 10
+
+# a  += 20
+# a -= 20
+# a *= 20
+
+# print(a)
+
+# Comparision
+
+# a = 10
+# b = 20
+# c = 10
+
+# print(a==b)
+# print(a>b)
+# print(a>=c)
+
+#logical
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# print(10>20 and 100<200)
+
+# print(not True)
+
+# identity
+
+a = [10,20,30,40,50]
+b = [1,2,3,4,5,6]
+c  = a
+
+# print(a  is not b)
+# print(c is a)
+
+
+# Membership 
+print(10 in a)
+print(100 in a)
+
