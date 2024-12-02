f5d21f161abcd7b3354d2ea31167d8f0d56d25e8
chintan <chintan.tops@gmail.com>
2024-12-02 16:07:13 +0530

    all

---
+++ statement/Ifdemo.py
@@ -0,0 +1,50 @@
+
+# age = 2
+
+
+# if age>18:
+#     print("Elegble for voting")
+# else:
+#     print("Not elegble for voting")
+
+a = 100
+b = 100
+c = 100
+
+
+# if a>b:
+#     print("a is greater")
+# else :
+#     print("b is greater")
+
+# if a>b:
+#     if a>c : 
+#         print("a is greater")
+#     else:
+#         print("c is greater")
+# else :
+#     if b>c:
+#         print("b is greater")
+#     else:
+#         print("c is greater")
+
+# if a>b and a>c :
+#     print("A is greater")
+# elif b>a and b>c:
+#     print("B is greater")
+# elif c>a and c>b:
+#     print("c is greater")
+# else:
+#     print("all are equal")
+
+
+# marks = 60
+# 91-100 A
+# 71-90 b
+# 51- 70 c
+# 35 - 50 d
+# 1-35 F
+
+
+
+
