3b3ba5a253ea687bdc3ffe5a0d08b4c11dcfe0f8
chintan <chintan.tops@gmail.com>
2024-12-25 15:54:41 +0530

    logical

---
+++ Logical/oddeven.py
@@ -0,0 +1,21 @@
+
+
+# for i in range(1,101):
+#     number = i
+
+#     if number%2==0:
+#         print(f"{number} : Even")
+#     else:
+#         print(f"{number} : Odd")
+
+i =1
+while(i<=100):
+
+    number = i
+
+    if number%2==0:
+        print(f"{number} : Even")
+    else:
+        print(f"{number} : Odd")
+    
+    i+=1
--- statement/looping.py
+++ statement/looping.py
@@ -10,3 +10,22 @@
 
 # for i in range(10,1,-2):
 #     print(i)
+
+# i=5
+# while(i<20):
+#     if i==10:
+#         continue
+#     print(i)
+#     i += 1
+
+# for i in range(20):
+#     if i==11:
+#         continue
+#     print(i)
+
+# a = 40
+
+# if a>20:
+#     print("somthing")
+# else :
+#     pass  
