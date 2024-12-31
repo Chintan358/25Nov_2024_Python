667ec4879baa0924aea7486bcf4b9d9aad80d352
chintan <chintan.tops@gmail.com>
2024-12-31 15:55:56 +0530

    string

---
+++ Logical/P1.py
@@ -0,0 +1,124 @@
+
+
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# for j in range(1,6):
+
+#     for i in range(1,6):
+#         print("*",end="")
+#     print()
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# for j in range(0,5):
+#     for i in range(0,j+1):
+#         print("*",end="")
+#     print()
+
+# starCount=1
+# lines = 10
+# for j in range(0,lines):
+
+#     for i in range(0,starCount):
+#         print("*",end="")
+#     print()
+#     starCount=starCount+1
+
+
+# *****
+# ****
+# ***
+# **
+# *
+
+# for i in range(0,5):
+#     for j in range(0,5-i):
+#         print("*",end="")
+#     print()
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# for j in range(0,5):
+
+#     for k in range(0,5-j):
+#         print(" ",end="")
+#     for i in range(0,j+1):
+#         print("*",end="")
+#     print()
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+# for j in range(0,5):
+
+#     for k in range(0,j):
+#         print(" ",end="")
+#     for i in range(0,5-j):
+#         print("*",end="")    
+#     print()
+
+
+#    *
+#   * *
+#  * * *
+# * * * *
+
+# for j in range(0,5):
+
+#     for k in range(0,5-j):
+#         print(" ",end="")
+#     for i in range(0,j+1):
+#         print("* ",end="")
+#     print()
+ 
+
+#    *
+#   ***
+#  *****
+# *******
+
+# for j in range(0,5):
+
+#     for k in range(0,5-j):
+#         print(" ",end="")
+#     for i in range(0,2*j+1):
+#         print("*",end="")
+#     print()
+
+#    *
+#   * *
+#  * * *
+#   * *
+#    *
+
+for j in range(0,5):
+
+    for k in range(0,5-j):
+        print(" ",end="")
+    for i in range(0,j+1):
+        print("* ",end="")
+    print()
+for j in range(1,5):
+
+    for k in range(0,j+1):
+        print(" ",end="")
+    for i in range(0,5-j):
+        print("* ",end="")    
+    print()
+
+
---
+++ String/stringpractical.py
@@ -0,0 +1,36 @@
+# print("tops tech")
+# print('tops tech')
+# print("""tops
+#       tech""")
+
+# text = "Sun Rises in east"
+
+# print(text.capitalize())
+# print(text.casefold())
+# print(text.center(25,'*'))
+# print(text.count("s",0,9))
+# print(text.endswith("s",0,9))
+# print(text.find("x",0,3))
+# print("my name is {1} and surname is {0}".format("Harsh","Chauhan"))
+# # print(text.index("x"))
+
+# print("abc".isalpha())
+# print("123".isdigit())
+# print("123".isdecimal())
+# print("&dsdsd".isidentifier())
+# print(text.islower())
+# print("Tops".istitle())
+# c = "abc".join("123")
+# print(c)
+# print(text.ljust(25,'#'))
+# print(text.partition("x"))
+# print(text.replace("s","T"))
+# print(text.split("s"))
+# print(text.swapcase())
+
+text = "Sun Rises in east"
+
+print(text[10])
+print(text[1:5])
+print(text[1:10:2])
+print(text[-5:-1])
